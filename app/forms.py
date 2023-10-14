baa362c82491534b2ae41ef9f5fa72d80feac6ff
AICHITANAKA <rqsaicbartlo@gmail.com>
2023-10-14 14:39:54 +0900

    これで動いてたはず

--- app/forms.py
+++ app/forms.py
@@ -23,10 +23,12 @@ class InquiryForm(forms.Form):
 
 class UploadForm(forms.Form):
     """アップロード用フォームの定義"""
-    upload_field = forms.FileField(label="画像アップロード",widget=MultiFileInput())
+    document = forms.FileField(label="画像アップロード",
+        widget=MultiFileInput(), 
+        )
 
     def save(self):
-        upload_files = self.files.getlist('upload_field')
+        upload_files = self.files.getlist('document')
         temp_dir = os.path.join(settings.MEDIA_ROOT, self.create_dir(10))   #一時フォルダの生成
         for image in upload_files:
             default_storage.save(os.path.join(temp_dir, image.name), image)   #一時フォルダに画像を保存
