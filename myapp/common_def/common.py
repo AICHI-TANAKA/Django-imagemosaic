64de2d6c5cd5ab8a1a45b3e5b412443e184c8290
AICHITANAKA <rqsaicbartlo@gmail.com>
2023-10-21 18:28:24 +0900

    modelに関数用意したけどマイグレーション実行時に実行されるだけだから意味ないっぽい。とりあえず現状は切り戻してこれで良しとする。

--- myapp/common_def/common.py
+++ myapp/common_def/common.py
@@ -3,6 +3,6 @@ from django.conf import settings
 
 def create_dir(n):
     """一時フォルダ名生成関数"""
-    return os.path.join(settings.MEDIA_ROOT, str(random.choices(string.ascii_letters + string.digits, k=n)))
-    # return 'image\\' + ''.join(random.choices(string.ascii_letters + string.digits, k=n))
+    # return os.path.join(settings.MEDIA_ROOT, str(random.choices(string.ascii_letters + string.digits, k=n)))
+    return 'image\\' + ''.join(random.choices(string.ascii_letters + string.digits, k=n))
 
--- myapp/models.py
+++ myapp/models.py
@@ -3,6 +3,6 @@ from .common_def.common import create_dir
 
 # Create your models here.
 class UploadedFile(models.Model):
-    file = models.FileField(upload_to=create_dir(10))   #一時フォルダの生成
+    file = models.FileField(upload_to='media/')   #一時フォルダの生成
     user_id = models.IntegerField()
     uploaded_at = models.DateTimeField(auto_now_add=True)
