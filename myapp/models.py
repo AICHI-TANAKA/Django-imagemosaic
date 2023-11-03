1d2cab1fe52611ee9598e6793a86100a9b044b3c
AICHITANAKA <rqsaicbartlo@gmail.com>
2023-11-03 16:08:10 +0900

    ユーザーidとペアになる画像idを追加　複合ユニークキーとする

--- myapp/models.py
+++ myapp/models.py
@@ -3,6 +3,25 @@ from .common_def.common import create_dir
 
 # Create your models here.
 class UploadedFile(models.Model):
+    """アップロードファイルの情報を格納するテーブル"""
     file = models.FileField(upload_to='mosbefore/'+create_dir(10)+"/")   #一時フォルダの生成
-    user_id = models.IntegerField()
+    user_id = models.IntegerField(null=False)
+    image_id = models.IntegerField(null=False)
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    
+    def save(self, *args, **kwargs):
+        if not self.image_id:
+            # user_idごとに最大のimage_idを取得
+            max_image_id = UploadedFile.objects.filter(user_id=self.user_id).aggregate(models.Max('image_id'))['image_id__max'] or 0
+            self.image_id = max_image_id + 1
+        super(UploadedFile, self).save(*args, **kwargs)
+    
+    class Meta:
+        """モデルのメタ情報。現時点では複合ユニークキーのみ設定"""
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user_id", "image_id"],
+                name="image_unique"
+            ),
+        ]
